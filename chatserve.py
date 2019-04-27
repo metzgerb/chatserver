7dbd8b8b206e8d1a69a8ba949a4bb352563d84bb
metzgerb <40550164+metzgerb@users.noreply.github.com>
2019-04-27 11:46:25 -0500

    Update chatserve.py
    added basic server receiving with infinite loops

--- chatserve.py
+++ chatserve.py
@@ -8,12 +8,36 @@ Created: 2019-04-27
 import socket
 import sys
 
-
+#function that creates the server and sets it to listen for clients
 def serve(port_number):
-    print("cmd arg recd: %s" % port_number)
+    #initialize message length
+    MAX_BUFFER = 700
+    
+    #set up server socket
+    server_socket = socket(AF_INET, SOCK_STREAM)
+    server_socket.bind(('', port_number))
+    
+    #continuously listen for a connection
+    while True:
+        server_socket.listen(1)
+        
+        #accept connection
+        connection_socket, addr = server_socket.accept()
+        
+        #continuously receive messages
+        while True:
+            #receive message from client
+            recd_message = connection_socket.recv(MAX_BUFFER).decode()
+            
+            #send message back
+            send_message = "CHATSERVE> Received message: %s" % recd_message
+            connection_socket.send(send_message.encode())
+        
+        #close connection
+        connection_socket.close()
+        
 
-
-
+#determine program execution
 if __name__ == "__main__":
     #check total argument count
     if len(sys.argv) != 2:
