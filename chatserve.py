132edb2b274a1220655516d1ee807f9bbd925980
metzgerb <40550164+metzgerb@users.noreply.github.com>
2019-05-02 19:49:38 -0500

    Update chatserve.py
    refactored server to be consist of modular functions

--- chatserve.py
+++ chatserve.py
@@ -10,8 +10,47 @@ Last Modified: 2019-05-01
 from socket import *
 import sys
 
+#funtion to send a message to a specified client
+def send_msg(client_socket, handle, sentinel):
+    #get input from user
+    message = input(handle)
+            
+    #check if message is "\quit"
+    if message == "\\quit":
+        #send quit message back with sentinel
+        message += sentinel
+        connection_socket.sendall(message.encode())
+        return -1          
+    else: 
+        #send message back
+        message = server_handle + message + sentinel
+        client_socket.sendall(message.encode())
+        return 0
+
+#function to receive a message from a specified client
+def recv_msg(client_socket, sentinel, max_buffer):
+    #set initial values to empty strings
+    buffer = ""
+    message = ""
+    
+    #call recv in a loop until sentinel is detected in compiled message
+    while(SENTINEL not in message):
+        buffer = client_socket.recv(max_buffer).decode()
+        message += buffer
+        
+    #remove sentinel from message
+    message = message.replace(sentinel, "")
+    
+    #check if socket has closed using length of recd message
+    if message == "\\quit":
+        return -1
+    else:    
+        #print message to server
+        print(message)
+        return message
+
 #function that creates the server and sets it to listen for clients
-def serve(port_number):
+def setup_server(port_number):
     #initialize values
     MAX_BUFFER = 700
     SENTINEL = "@!@"
@@ -30,37 +69,19 @@ def serve(port_number):
 
         #continuously receive and send messages
         while True:
-            recd_message = ""
-            buffer = ""
+            #receive a message from client
+            recd_message = recv_msg(connection_socket, SENTINEL, 700)
             
-            #receive message from client (loops until full message is received
-            while(SENTINEL not in recd_message):
-                buffer = connection_socket.recv(MAX_BUFFER).decode()
-                recd_message += buffer
-            
-            #remove sentinel
-            recd_message = recd_message.replace(SENTINEL, "")
-            
-            #check if socket has closed using length of recd message
-            if recd_message == "\\quit":
+            #check if received message was quit
+            if recd_message == -1:
                 break
             
-            #print message to server
-            print(recd_message)
+            #send message to client
+            send_message = send_msg(connection_socket, server_handle, SENTINEL)
             
-            #get input from user
-            message = input(server_handle)
-            
-            #check if message is "\quit"
-            if message == "\\quit":
-                #send quit message back
-                send_message = "\\quit" + SENTINEL
-                connection_socket.sendall(send_message.encode())
-                break          
-            
-            #send message back
-            send_message = server_handle + message + SENTINEL
-            connection_socket.sendall(send_message.encode())
+            #check if sent message was quit
+            if send_message == -1:
+                break
         
         #close connection
         connection_socket.close()
@@ -74,4 +95,4 @@ if __name__ == "__main__":
     elif not sys.argv[1].isdigit():
         print("syntax error: port must be a number")
     else:
-        serve(int(sys.argv[1]));
+        setup_server(int(sys.argv[1]));
